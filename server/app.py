bd38a1c86e247585e95efc44864bd339a9fd69c9
Allan Kinuva <allankinuva@gmail.com>
2025-06-11 14:35:59 +0300

    final commit

--- server/app.py
+++ server/app.py
@@ -1,5 +1,24 @@
-#!/usr/bin/env python3
-
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+@app.route('/request-demo')
+def request_demo():
+    user_agent = request.headers.get('User-Agent')  
+    method = request.method  
+    return f"""
+        <h1>Request Info</h1>
+        <p><strong>HTTP Method:</strong> {method}</p>
+        <p><strong>User Agent:</strong> {user_agent}</p>
+    """
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
